0e16a002738b059fac7059fff10026692cbcd781
ApurvJDev <jadhavapurv34@gmail.com>
2024-07-14 21:32:24 +0530

    added routing in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/ApurvJDev/goMgmtSys/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+// we are going to create a server ie define the localhost
+// we are going to tell where our routers reside
+
+func main() {
+	router := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(router)
+
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe()) // helps us create the server
 }
